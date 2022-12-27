3553734aedb119509f91362a582ed77523d36236
IgnacioPerezEtc <nachoperezetc@gmail.com>
2022-12-27 16:36:17 -0300

    actualizando cambios en el index

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
+require('dotenv').config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const port = process.env.DB_PORT || 3001;
+const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
-    console.log("%s listening at", port); // eslint-disable-line no-console
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
